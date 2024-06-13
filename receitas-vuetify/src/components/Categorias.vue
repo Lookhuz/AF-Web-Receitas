5db1b494e597f40bae4032923118e423d82d6297
Lucas Silveira <lucasdmsilveira@gmail.com>
2024-06-13 11:34:02 -0300

    Display category data and implement category data fetching

--- receitas-vuetify/src/components/Categorias.vue
+++ receitas-vuetify/src/components/Categorias.vue
@@ -17,9 +17,39 @@
       xl="10"
       class="pt-8"
     >
-      <div class="text-h3 font-weight-light mb-10 pb-8 text-center">
-        {{ category }}
-      </div>  
+      <div v-if="categoryData" class="text-h3 font-weight-light pb-8 text-center">
+        {{ categoryData.name }}
+      </div>
+
+      <div>
+        <div v-if="categoryData">
+            <v-row justify="space-around">
+              <v-col
+                v-for="key in categoryData.receitas"
+                :key="key"
+                cols="12"
+                md="4"
+                >
+                <div
+                class="cursor-pointer"
+                @click="navigateToReceipt(key)"
+                >
+                  <v-img
+                    style="height: 200px;"
+                    :src="key.imgLink"
+                    cover
+                    class="my-auto"
+                  >
+                  </v-img>
+                  
+                  <div class="text-subtitle-1 mb-2 text-center">
+                    <span>{{ key.name }}</span>
+                  </div>
+                </div>
+              </v-col>
+            </v-row>
+          </div>
+      </div>
     </v-col>
 
     <v-col
@@ -35,12 +65,31 @@
 
 <script>
 export default {
-  computed: {
-    category() {
-      return this.$route.query.category;
+  data() {
+    return {
+      categoryData: null
     }
+  },
+  methods: {
+    getCategoryData() {
+      const category = this.$route.query.categoryString;
+      if (category) {
+        this.categoryData = JSON.parse(category);
+      } else {
+        console.log('Category data is not defined');
+      }
+    },
+    navigateToReceipt(receipt) {
+      const receiptString = JSON.stringify(receipt);
+      this.$router.push({ path: '/receita', query: { receiptString } });
+    }
+  },
+  mounted () {
+    this.getCategoryData();
   }
 }
 </script>
+
+
 <style>
 </style>
