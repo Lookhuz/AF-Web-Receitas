eabf930be1c21fb9b03edc27c1b0df1511e32daf
Lucas Silveira <lucasdmsilveira@gmail.com>
2024-06-13 11:36:45 -0300

    Integrate footer component into App layout

--- receitas-vuetify/src/layouts/siteLayout.vue
+++ receitas-vuetify/src/layouts/siteLayout.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
     <navbar></navbar>
-
+    
     <v-main>
       <router-view />
     </v-main>
+    
+    <Footer/>
   </div>
 </template>
 
 <script>
 import Navbar from "./../components/NavBar.vue"
+import Footer from "./../components/Footer.vue"
 
 export default {
   components: {
-    Navbar
+    Navbar,
+    Footer
   }
 }
 </script>
